5905fb1b902cb6521b8aa02ac4fb3a83ea1a5312
zhaoyu wang <zhaoyu@kth.se>
2015-07-09 16:21:50 +0800

    取消viewpager

--- app/src/main/java/com/example/wangzhaoyu/myguokr/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/wangzhaoyu/myguokr/ui/activity/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -14,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import com.example.wangzhaoyu.myguokr.R;
 import com.example.wangzhaoyu.myguokr.server.ArticleServer;
@@ -30,18 +33,11 @@ import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
     private static String TAG = MainActivity.class.getSimpleName();
-
-    @InjectView(R.id.viewpager)
-    ViewPager mViewPager;
-
-    @InjectView(R.id.tab)
-    TabLayout mTabLayout;
+    @InjectView(R.id.fragment_container)
+    FrameLayout mFrameLayout;
 
     @InjectView(R.id.toolbar)
     Toolbar mToolbar;
-
-    @InjectView(R.id.fab)
-    FloatingActionButton mFab;
 
     @InjectView(R.id.main_drawer)
     DrawerLayout mDrawerLayout;
@@ -72,33 +68,10 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        //set up fab
-        mFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Snackbar.make(v, "here I am", Snackbar.LENGTH_SHORT)
-                        .setAction("Action", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-
-                            }
-                        }).show();
-            }
-        });
-
-        //set up viewpager
-        List<String> titiles = new ArrayList<>();
-        titiles.add("科学人");
-        titiles.add("果壳小组");
-        titiles.add("果壳问答");
-        List<Fragment> fragments = new ArrayList<>();
-        fragments.add(new ScientificFragment());
-        fragments.add(new PagerTextFragment());
-        fragments.add(new PagerTextFragment());
-        TabViewPagerAdapter adapter = new TabViewPagerAdapter(getSupportFragmentManager(), fragments, titiles);
-        mViewPager.setAdapter(adapter);
-        mTabLayout.setupWithViewPager(mViewPager);
-        mTabLayout.setTabsFromPagerAdapter(adapter);
+        //set up main fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, new ScientificFragment()).commit();
     }
 
     @Override
