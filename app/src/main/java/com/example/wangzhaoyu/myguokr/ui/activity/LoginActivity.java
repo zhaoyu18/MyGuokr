bd0eef0e5c1a04568f70b7aeae33dfd56f55875b
zhaoyu wang <zhaoyu@kth.se>
2015-07-10 16:43:25 +0800

    image title black cover & login beginning

---
+++ app/src/main/java/com/example/wangzhaoyu/myguokr/ui/activity/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.wangzhaoyu.myguokr.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.wangzhaoyu.myguokr.R;
+
+/**
+ * @author wangzhaoyu
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
